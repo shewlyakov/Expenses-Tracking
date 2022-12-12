90c87ffa35ecc7b889d1c1a5a2d26f37d9a69191
shewlyakov <oleksandrsevlakov@gmail.com>
2022-12-12 10:15:45 +0200

    Another change

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def create
     @current_user = current_user
-    @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+    @message = @current_user.messages.create(content: "Мої витрати:<br /><br /> #{expenses}", room_id: params[:room_id])
   end
 
   private
